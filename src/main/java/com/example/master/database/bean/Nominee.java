eecf46af4cd5740cab603d79dd408dc429958d49
Rohit Raman <rohitraman9944@gmail.com>
2024-01-24 20:43:01 +0530

    Done

--- src/main/java/com/example/master/database/bean/Nominee.java
+++ src/main/java/com/example/master/database/bean/Nominee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Nominee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(nullable = false)
